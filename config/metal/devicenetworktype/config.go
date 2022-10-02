f2d312809d4d66dd38e98c7ad92facf5351853a2
Marques Johansson <mjohansson@equinix.com>
2022-10-01 23:09:54 -0400

    add referencers for project_id in several Metal types
    Signed-off-by: Marques Johansson <mjohansson@equinix.com>

---
+++ config/metal/devicenetworktype/config.go
@@ -0,0 +1,30 @@
+/*
+Copyright 2021 The Crossplane Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package devicenetworktype
+
+import (
+	"github.com/crossplane/terrajet/pkg/config"
+)
+
+// Configure the devicenetworktype group with references to other resources
+func Configure(p *config.Provider) {
+	p.AddResourceConfigurator("equinix_metal_device_network_type", func(r *config.Resource) {
+		r.References["device_id"] = config.Reference{
+			Type: "Device",
+		}
+	})
+}
