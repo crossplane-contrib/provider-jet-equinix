84191bec9e04f58ce3c222c6780a510ed8342a79
Marques Johansson <mjohansson@equinix.com>
2024-02-06 13:06:56 +0000

    chore(deps): update crossplane-tools, crossplane-runtime to 1.14.1
    Signed-off-by: Marques Johansson <mjohansson@equinix.com>

--- internal/features/features.go
+++ internal/features/features.go
@@ -1,5 +1,6 @@
 /*
  Copyright 2022 The Crossplane Authors.
+ Copyright 2022 Upbound Inc
  Licensed under the Apache License, Version 2.0 (the "License");
  you may not use this file except in compliance with the License.
  You may obtain a copy of the License at
@@ -13,12 +14,20 @@
 
 package features
 
-import "github.com/crossplane/crossplane-runtime/pkg/feature"
+import (
+	xpfeature "github.com/crossplane/crossplane-runtime/pkg/feature"
+)
 
 // Feature flags.
 const (
 	// EnableAlphaExternalSecretStores enables alpha support for
 	// External Secret Stores. See the below design for more details.
 	// https://github.com/crossplane/crossplane/blob/390ddd/design/design-doc-external-secret-stores.md
-	EnableAlphaExternalSecretStores feature.Flag = "EnableAlphaExternalSecretStores"
+	EnableAlphaExternalSecretStores xpfeature.Flag = "EnableAlphaExternalSecretStores"
+
+       // EnableBetaManagementPolicies enables beta support for
+       // Management Policies. See the below design for more details.
+       // https://github.com/crossplane/crossplane/pull/3531
+       EnableBetaManagementPolicies xpfeature.Flag = xpfeature.EnableBetaManagementPolicies
+
 )
